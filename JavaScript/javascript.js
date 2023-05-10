66f09976d22822304256de292f280cf35b7c4fae
Altaf Shaik <100480476+Altaf-Shaik@users.noreply.github.com>
2023-05-10 12:25:44 +0530

    Add files via upload

---
+++ JavaScript/javascript.js
@@ -0,0 +1,7 @@
+function clicked() {
+    const ele = document.querySelector("img");
+    if(ele.classList.contains('shake'))
+        window.open("https://amzn.to/41lLOUU");
+    else if(ele.classList.contains('streetLawyer'))
+        window.open("https://amzn.to/3HZ0J06");
+}
